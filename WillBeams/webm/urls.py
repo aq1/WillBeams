ce7527e8567c97f13b7a3c4647d993207bad0e5b
pawnhearts <pawn13@gmail.com>
2015-09-30 15:39:59 +0300

    urls fixed for rest framework

--- WillBeams/webm/urls.py
+++ WillBeams/webm/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from rest_framework import routers
 
-from .views import index
+from . import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
