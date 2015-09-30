9130ae4d8039ffb0c08b03f50ff025b67b609e0c
pawnhearts <pawn13@gmail.com>
2015-09-30 13:10:40 +0300

    get_top_tags

--- WillBeams/webm/models/webm_tag.py
+++ WillBeams/webm/models/webm_tag.py
@@ -12,3 +12,8 @@ class WebmTag(models.Model):
 
     def __str__(self):
         return '{}: {}'.format(self.tag, self.webm)
+
+    @classmethod
+    def get_top_tags(cls, count=10):
+        return cls.objects.values('tag__name').annotate(count=Count('webm')).order_by('-count')[:count]
+
