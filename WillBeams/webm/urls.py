6829c705a4aeaca713df8036aa94b774c5dd9043
pawnhearts <pawn13@gmail.com>
2015-09-30 15:02:34 +0300

    rest framework added

--- WillBeams/webm/urls.py
+++ WillBeams/webm/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import url
+from rest_framework import routers
 
 from .views import index
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'tags', views.TagViewSet)
+router.register(r'webm', views.WebmViewSet)
 
 urlpatterns = [
-    url(r'', index),
+    #url(r'', index),
+    url(r'', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
--- WillBeams/webm/views/__init__.py
+++ WillBeams/webm/views/__init__.py
@@ -1,1 +1,37 @@
 from .index import index
+
+from django.contrib.auth.models import User, Group
+from webm.models import Tag, Webm
+from rest_framework import viewsets
+from webm.serializers import UserSerializer, GroupSerializer, TagSerializer, WebmSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class TagViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+class WebmViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Webm.objects.all()
+    serializer_class = WebmSerializer
+
