34fd420f505635fe6bbf1654a0e2e10bbfd5d590
aq1 <inmosh@yandex.ru>
2015-10-02 10:06:52 +0300

    PEP 8

--- WillBeams/webm/serializers.py
+++ WillBeams/webm/serializers.py
@@ -4,23 +4,28 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups')
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Group
         fields = ('url', 'name')
 
+
 class TagSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Tag
-        fields = ('name','webm_set')
+        fields = ('name', 'webm_set')
+
 
 class WebmSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Webm
         fields = ('thumbnail', 'rating', 'nsfw', 'tags', 'added')
-
