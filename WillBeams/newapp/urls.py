79d4e9c7fcf2a0bee6a146bc59c91ed7a6972714
kctier <joe.tester.j@gmail.com>
2015-10-05 02:58:15 +0300

    Uniform all auth templates

--- WillBeams/newapp/urls.py
+++ WillBeams/newapp/urls.py
@@ -15,10 +15,25 @@ urlpatterns = [
         name='password_change'
     ),
     url(r'^password_change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
-    url(r'^password_reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
-    url(r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
+    url(
+        r'^password_reset/$',
+        'django.contrib.auth.views.password_reset',
+        {'template_name': 'registration/password_reset.html'},
+        name='password_reset'
+    ),
+    url(
+        r'^password_reset/done/$',
+        'django.contrib.auth.views.password_reset_done',
+        {'template_name': 'registration/password_reset_done.html'},
+        name='password_reset_done'
+    ),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
-    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
+    url(
+        r'^reset/done/$',
+        'django.contrib.auth.views.password_reset_complete',
+        {'template_name': 'registration/password_reset_complete.html'},
+        name='password_reset_complete'
+    ),
 
 ]
