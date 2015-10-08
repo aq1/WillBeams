bb139dc1eb64ded623755929614d6bb79c0a8058
pawnhearts <pawn13@gmail.com>
2015-10-08 18:53:41 +0300

    some new columns in webm admin

--- WillBeams/newapp/admin.py
+++ WillBeams/newapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.core.urlresolvers import reverse
 
-from newapp.models import Webm, Tag
+from newapp.models import Webm, Tag, View
 
 class TagInlineAdmin(admin.TabularInline):
     model = Webm.tag.through
@@ -9,10 +9,19 @@ class TagInlineAdmin(admin.TabularInline):
 @admin.register(Webm)
 class WebmAdmin(admin.ModelAdmin):
     readonly_fields = ['added']
-    list_display = ['thumb_img', 'added']
+    list_display = ['thumb_img', 'added', 'likes_count', 'views_count', 'tag_list']
     list_filter = ['tag__name',]
     #fields = ['video', 'thumb', 'length', 'added', 'nsfw_source', 'blacklisted']
     inlines = (TagInlineAdmin,)
+
+    def likes_count(self, obj):
+        return obj.likes.count()
+
+    def views_count(self, obj):
+        return View.objects.filter(webm=obj).count()
+
+    def tag_list(self, obj):
+        return ','.join(obj.tag.values_list('name', flat=True))
 
     def thumb_img(self, obj):
         html = '<img style="max-width:100%%; \
