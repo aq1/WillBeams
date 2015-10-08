3f33eaab8febd05edb77a9c07b71ea5b7fc47c03
egorpuzyrev <egorpuzyrev@gmail.com>
2015-10-09 00:01:47 +0300

    Some bugfixies. Replaced '-vf fps 1/count' with '-r 1/count'. Added crop filter. New FF.webm_info function which can be used with 'with'.

--- videoproc/ff.py
+++ videoproc/ff.py
@@ -10,7 +10,8 @@ import json
 
 from distutils.spawn import find_executable
 
-import sfml as sf
+import time
+# ~import sfml as sf
 from sys import getsizeof as size
 
 DEFAULT_FFMPEG_BIN = 'ffmpeg' if not platform.system() == 'Window' else 'ffmpeg.exe'
@@ -20,7 +21,7 @@ DEFAULT_FF_PATH = './ffmpeg/' if os.path.isdir('./ffmpeg') and os.path.exists('.
 REQUIRED_FFPROBE_OPTIONS = ['-hide_banner', '-v', 'quiet', '-print_format', 'json=c=1']
 DEFAULT_FFPROBE_OPTIONS = ['-show_format', '-show_streams']
 
-DEFAULT_FFMPEG_OPTIONS = ['-hide_banner', '-v', 'quiet']
+DEFAULT_FFMPEG_OPTIONS = ['-hide_banner', '-v', 'quiet', '-an']
 
 THUMBS_DIR = '/tmp/ram/'
 
@@ -34,33 +35,42 @@ EMPTY_VIDEO = {
 EMPTY_AUDIO = {
                 'codec_name': None
                 }
-                
+
 
 class FFInfo(object):
-    
+
     def __init__(self, path, file_info):
-                  
+
         self.path = path
-        
+
         file_format = file_info['format']
-        
+
         videostreams = [ x for x in file_info['streams'] if x['codec_type']=='video' ]
         audiostreams = [ x for x in file_info['streams'] if x['codec_type']=='audio' ]
-        
+
         videostream = videostreams[0] if videostreams else EMPTY_VIDEO
         audiostream = audiostreams[0] if audiostreams else EMPTY_AUDIO
-        
+
         self.videostream = videostream
         self.audiostream = audiostream
-        
+
         self.videocodec = videostream['codec_name']
         self.audiocodec = audiostream['codec_name']
-        
+
         self.height = int(videostream['height'])
         self.width = int(videostream['width'])
         self.duration = float(file_format['duration'])
         self.filename = os.path.basename(file_format['filename'])
-        
+
+        self.thumbs = []
+
+    def __enter__(self):
+
+        return self
+
+    def __exit__(self, typeof, value, traceback):
+
+        pass
 
 class FF(object):
 
@@ -87,7 +97,7 @@ class FF(object):
             self._store = lambda filename, file_info: ffstorage.store(filename, file_info)
         else:
             self._store = lambda x, y: None
-            
+
     def get_file_info(self, filename, options=DEFAULT_FFPROBE_OPTIONS):
 
         if not self._ffstorage is None and filename in self._ffstorage._storage:
@@ -95,13 +105,13 @@ class FF(object):
             return ffinfo
 
         ff_options = [DEFAULT_FFPROBE_BIN, '-i', filename] + REQUIRED_FFPROBE_OPTIONS + options
-        
+
         try:
             file_info = json.loads(subprocess.check_output(ff_options).decode('utf-8'))
         except:
             pass
         ffinfo = FFInfo(filename, file_info)
-                
+
         return ffinfo
 
     def load_file_info(self, filename, options=DEFAULT_FFPROBE_OPTIONS):
@@ -110,48 +120,85 @@ class FF(object):
 
         if not self._ffstorage is None:
             self._store(filename, ffinfo)
-            
+
         return ffinfo
 
-    def generate_thumbs(self, filename, minstep=1, count=1, thumbs_dir=os.path.abspath(THUMBS_DIR), width=-1, height=-1):
+    def generate_thumbs(self, filename, minstep=1, count=1, thumbs_dir=os.path.abspath(THUMBS_DIR), width=-1, height=-1, cwidth=0, cheight=0):
 
         ffinfo = self.get_file_info(filename, DEFAULT_FFPROBE_OPTIONS)
 
-        duration = int(ffinfo.duration)
+        duration = float(ffinfo.duration)
 
         cur_file = os.path.join(thumbs_dir, ffinfo.filename[:-5])
-        
-        step = max(minstep, duration/(count))
-        
+
+        # ~step = max(minstep, duration/(count))
+        step = duration/(count)
+
         fps = '1/'+str(step)
+
+        # ~ss = step
+        ss = step if count%2 else step//2
+         
+        fps_f = 'fps='+str(fps)
+        scale_f = 'scale='+str(width)+':'+str(height)
+        crop_f = "crop=w='min(iw,"+str(cwidth)+")':h='min(ih,"+str(cheight if cheight else cwidth)+")'"
         
-        ss = step
+        select_f = 'select=gt(scene\,0.1)'
 
-        args = [DEFAULT_FFMPEG_BIN, '-i', filename, '-ss', str(ss), '-vf', 'fps='+str(fps)+', '+'scale='+str(width)+':'+str(height),  '-f', 'image2'] + DEFAULT_FFMPEG_OPTIONS + [cur_file+'_thumb_%d.jpg']
+        # ~vfilters = fps_f+', '+scale_f
+        vfilters = scale_f+', '+crop_f
 
-        print(args)
+        thumb_postfix = """_thumb_%02d.jpg"""
+
+        args = [DEFAULT_FFMPEG_BIN,
+                '-i', filename,
+                '-ss', str(ss),
+                '-vf', str(vfilters),
+                '-r', str(1/(step+1)),
+                # ~'-vsync', 'vfr',
+                # ~'-vframes', str(count+3),
+                '-f', 'image2'] \
+                + DEFAULT_FFMPEG_OPTIONS \
+                + [cur_file+thumb_postfix]
 
         try:
-            subprocess.call(args)        
+            subprocess.check_call(args)
+            ffstorage = self.get_file_info(filename)
+            ffstorage.thumbs.extend([cur_file+thumb_postfix %(i,) for i in range(1, count+1)])
         except:
             print("ERROR CREATING THUMB")
+
+
+    def webm_info(self, filename, minstep, count, **kwargs):
+
+        ffinfo = self.load_file_info(filename)
+
+        if ffinfo.thumbs == []:
+            self.generate_thumbs(filename, minstep=minstep, count=count)
+
+        return ffinfo
 
 
 class FFStorage(object):
 
     def __init__(self):
-        
+
         self._storage = {}
 
     def store(self, filename, ffinfo):
-        
+
         self._storage[filename] = ffinfo
 
     def get(self, filename):
 
         return self._storage.get(filename)
 
+    def set(self, filename, **kwargs):
 
+        ffinfo = self._storage.get(filename)
+
+        if not ffinfo is None:
+            ffinfo.__dict__.update(kwargs)
 
 if __name__ == '__main__':
 
@@ -163,33 +210,52 @@ if __name__ == '__main__':
     webms = [ os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.lower().endswith('.webm') ][0:3]
     files = [ f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) ]
 
-    print(">>>LOAD FILES INFO")
+    print(">>>LOADING FILES INFO")
 
-    clock = sf.Clock()
-    last_time = 0
-    last_time = clock.elapsed_time.milliseconds
+    # ~clock = sf.Clock()
+    # ~last_time = 0
+    # ~clast_time = clock.elapsed_time.milliseconds
+    last_time = time.process_time()
 
     for webm in webms:
         ff.load_file_info(webm)
-        
-    current_time = clock.elapsed_time.milliseconds
+
+    # ~ccurrent_time = clock.elapsed_time.milliseconds
+    current_time = time.process_time()
 
     delta_time = current_time - last_time
-    print("TOTALLY: %d ms, %d webms" %(delta_time, len(webms)))
-    print("<MILLISECONDS> PER WEBM:", delta_time/len(webms))
+    # ~cdelta_time = ccurrent_time - clast_time
+    print("TOTALLY (time): %d ms, %d webms" %(delta_time*1000, len(webms)))
+    # ~print("TOTALLY (sfml): %d ms, %d webms" %(cdelta_time, len(webms)))
+    print("<MILLISECONDS> (time) PER WEBM:", delta_time*1000/len(webms))
+    # ~print("<MILLISECONDS> (sfml) PER WEBM:", cdelta_time/len(webms))
 
 
     print(">>>THUMBS GEN")
 
-    clock = sf.Clock()
-    last_time = 0
-    last_time = clock.elapsed_time.milliseconds
+    # ~clock = sf.Clock()
+    # ~last_time = 0
+    # ~clast_time = clock.elapsed_time.milliseconds
+    last_time = time.process_time()
 
-    for webm in webms:
-        ff.generate_thumbs(webm, width=500, height=100)
-        
-    current_time = clock.elapsed_time.milliseconds
+    for ww in enumerate(webms):
+        webm = ww[1]
+        ff.generate_thumbs(webm, count=5, width=600, cwidth=400)
+
+        with ff.webm_info(webm, minstep=10, count=5) as w:
+            print()
+            print(w.filename)
+            print(w.path)
+            print(w.duration, w.width, w.height)
+            for i in w.thumbs:
+              print(i)
+
+    # ~ccurrent_time = clock.elapsed_time.milliseconds
+    current_time = time.process_time()
 
     delta_time = current_time - last_time
-    print("TOTALLY: %d ms, %d webms" %(delta_time, len(webms)))
-    print("<MILLISECONDS> PER WEBM:", delta_time/len(webms))
+    # ~cdelta_time = ccurrent_time - clast_time
+    print("TOTALLY (time): %d ms, %d webms" %(delta_time*1000, len(webms)))
+    # ~print("TOTALLY (sfml): %d ms, %d webms" %(cdelta_time, len(webms)))
+    print("<MILLISECONDS> (time) PER WEBM:", delta_time*1000/len(webms))
+    # ~print("<MILLISECONDS> (sfml) PER WEBM:", cdelta_time/len(webms))
