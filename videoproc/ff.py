c372606cea8c431e56123a252ecbe09109d48263
kctier <joe.tester.j@gmail.com>
2015-10-16 07:43:14 +0300

    FF code cleanup

--- videoproc/ff.py
+++ videoproc/ff.py
@@ -1,33 +1,21 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import os
 import sys
 import platform
 import subprocess
-
 import tempfile
-
 import json
-
 import time
-
 from math import floor
 
-FFMPEG_BIN = 'ffmpeg' if not platform.system() == 'Window' else 'ffmpeg.exe'
-FFPROBE_BIN = 'ffprobe' if not platform.system() == 'Window' else 'ffprobe.exe'
-DEFAULT_FF_PATH = './ffmpeg/' if os.path.isdir('./ffmpeg') and os.path.exists('./ffmpeg/'+FFMPEG_BIN) else ""
 
-DEFAULT_FFMPEG_BIN = os.path.join(DEFAULT_FF_PATH, FFMPEG_BIN)
-DEFAULT_FFPROBE_BIN = os.path.join(DEFAULT_FF_PATH, FFPROBE_BIN)
+FFMPEG_BIN = os.environ.get('FFMPEG') or ('ffmpeg' if not platform.system() == 'Window' else 'ffmpeg.exe')
+FFPROBE_BIN = os.environ.get('FFPROBE') or ('ffprobe' if not platform.system() == 'Window' else 'ffprobe.exe')
 
-REQUIRED_FFPROBE_OPTIONS = ['-hide_banner', '-print_format', 'json=c=1']  # , '-v', 'quiet']
+REQUIRED_FFPROBE_OPTIONS = ['-hide_banner', '-print_format', 'json=c=1']
 DEFAULT_FFPROBE_OPTIONS = ['-show_format', '-show_streams']
 
-DEFAULT_FFMPEG_OPTIONS = ['-pix_fmt', 'yuvj420p', '-hide_banner', '-an']  # , '-v', 'quiet']
-
-THUMBS_DIR = tempfile.TemporaryDirectory()
-THUMBS_DIR_NAME = THUMBS_DIR.name
+DEFAULT_FFMPEG_OPTIONS = ['-pix_fmt', 'yuvj420p', '-hide_banner', '-an']
 
 EMPTY_VIDEO = {
     'height': 0,
@@ -42,10 +30,10 @@ EMPTY_AUDIO = {
 
 
 class FFInfo(object):
-
+    'dummy object for storing ffprobe info'
     def __init__(self, path, file_info):
-
         self.path = path
+        self.filename = os.path.basename(self.path)
 
         file_format = file_info['format']
 
@@ -64,80 +52,55 @@ class FFInfo(object):
         self.height = int(videostream['height'])
         self.width = int(videostream['width'])
         self.duration = float(file_format['duration'])
-        self.filename = os.path.basename(file_format['filename'])
 
 
-def get_file_info(filename, options=DEFAULT_FFPROBE_OPTIONS):
-
-    ff_options = \
-        [DEFAULT_FFPROBE_BIN, '-i', filename] \
-        + REQUIRED_FFPROBE_OPTIONS \
-        + options
-
-    file_info = json.loads(subprocess.check_output(ff_options).decode('utf-8'))
-
-    return file_info
-
-
-def generate_thumbs(
+def get_file_info(filename, options=DEFAULT_FFPROBE_OPTIONS, quiet=True):
+    ff_options = [FFPROBE_BIN, '-i', filename] + REQUIRED_FFPROBE_OPTIONS + options
+    if quiet:
+        ff_options.extend(['-v', 'quiet'])
+    return FFInfo(
         filename,
-        minstep=1,
-        count=1,
-        vfilters="",
-        thumbs_dir=os.path.abspath(THUMBS_DIR_NAME)
-):
+        json.loads(subprocess.check_output(ff_options).decode('utf-8'))
+    )
 
-    ffinfo = FFInfo(filename, get_file_info(filename, DEFAULT_FFPROBE_OPTIONS))
 
-    cur_file = os.path.join(thumbs_dir, ffinfo.filename[:-5])
+def generate_thumbs(ffinfo, thumbs_dir, minstep=1, count=1, vfilters=None, quiet=True):
+    fbase, _ = os.path.splitext(ffinfo.filename)
+    fbase += '_thumb_'
+    fend = '.jpg'
+    fname_template = os.path.join(thumbs_dir, fbase + '%02d' + fend)
+    time_params = rate_dur(ffinfo.duration, count, minstep)
 
-    duration = float(ffinfo.duration)
+    def tfile_list():
+        return [f for f in os.listdir(thumbs_dir) if f.startswith(fbase) and f.endswith(fend)]
 
-    time_params = rate_dur(duration, count, minstep)
-
-    ss = time_params['-ss']
-    t = time_params['-t']
-    r = time_params['-r']
+    assert not tfile_list(), 'You must clean target directory before generating thumbnails'
 
     # ~fps_f = 'fps='+str(fps)
     # ~select_f = 'select=gt(scene\,0.1)'
 
-    thumb_postfix = '_thumb_%02d.jpg'
-
     args = [
-        DEFAULT_FFMPEG_BIN,
-        '-i', filename,
-        '-ss', str(ss),
-        '-vf' if vfilters else '', str(vfilters),
-        '-r', str(r),
-        '-t', str(t),
+        FFMPEG_BIN,
+        '-i', ffinfo.path,
+        '-ss', str(time_params['-ss']),
+    ]
+    if vfilters is not None:
+        args.extend(['-vf', vfilters])
+    args.extend([
+        '-r', str(time_params['-r']),
+        '-t', str(time_params['-t']),
         '-f', 'image2'
-    ] \
-        + DEFAULT_FFMPEG_OPTIONS \
-        + [cur_file+thumb_postfix]
+    ])
+    args.extend(DEFAULT_FFMPEG_OPTIONS)
+    if quiet:
+        args.extend(['-v', 'quiet'])
+    args.append(fname_template)
 
     subprocess.check_call(args)
-
-    thumbs_list = [os.path.join(thumbs_dir, f) for f in os.listdir(thumbs_dir)]
-
-    return thumbs_list
-
-
-def webm_info(filename):
-
-    return FFInfo(get_file_info(filename))
-
-
-class WebmInfo(FFInfo):
-
-    def __init__(self, filename):
-
-        file_info = get_file_info(filename)
-        FFInfo.__init__(self, filename, file_info)
+    return tfile_list()
 
 
 def rate_dur(duration, count=1, cminstep=5):
-
     gap_length = duration / count
     gap_length = max(cminstep, gap_length)
     gap_length = min(duration, gap_length)
@@ -158,68 +121,57 @@ def rate_dur(duration, count=1, cminstep=5):
 
 
 if __name__ == '__main__':
-
     path = sys.argv[1]
-
-    webms = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.lower().endswith('.webm')]
-    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
-
+    webms = [
+        os.path.join(path, f) for f in os.listdir(path)
+        if os.path.isfile(os.path.join(path, f)) and f.lower().endswith('.webm')
+    ]
     print(">>>LOADING FILES INFO")
 
-    last_time = time.process_time()
+    last_time = time.monotonic()
 
     for webm in webms:
         get_file_info(webm)
 
-    current_time = time.process_time()
+    delta_time = time.monotonic() - last_time
 
-    delta_time = current_time - last_time
-
-    print("TOTALLY (time): %d ms, %d webms" % (delta_time*1000, len(webms)))
-    print("<MILLISECONDS> (time) PER WEBM:", delta_time*1000/len(webms))
-
+    print('TOTALLY (time): {} ms, {} webms'.format(delta_time * 1000, len(webms)))
+    print("<MILLISECONDS> (time) PER WEBM:", delta_time * 1000 / len(webms))
     print("\n\n\n")
 
-    print(">>>THUMBS GEN\n")
+    print(">>>THUMBS GEN")
 
-    last_time = time.process_time()
+    last_time = time.monotonic()
 
     width = height = 600
     cwidth = cheight = 400
 
-    temp_dir = tempfile.TemporaryDirectory()
-    temp_dir_name = temp_dir.name
-
     for webm in webms:
         print(">>>WEBM", webm)
+        with tempfile.TemporaryDirectory() as outdir:
+            w = get_file_info(webm)
+            print(w.filename)
+            print(w.path)
+            print(w.duration, w.width, w.height)
 
-        w = WebmInfo(webm)
+            scale_f = 'scale='+str(width)+':'+str(height)
+            crop_f = "crop=w='min(iw,"+str(cwidth)+")':h='min(ih,"+str(cheight if cheight else cwidth)+")'"
+            vfilters = scale_f+', '+crop_f
 
-        print(w.filename)
-        print(w.path)
-        print(w.duration, w.width, w.height)
+            thumbs = generate_thumbs(
+                w,
+                outdir,
+                minstep=1,
+                count=5,
+                vfilters=vfilters
+            )
 
-        scale_f = 'scale='+str(width)+':'+str(height)
-        crop_f = "crop=w='min(iw,"+str(cwidth)+")':h='min(ih,"+str(cheight if cheight else cwidth)+")'"
-
-        vfilters = scale_f+', '+crop_f
-
-        thumbs = generate_thumbs(
-            webm,
-            minstep=1,
-            count=5,
-            vfilters=vfilters,
-            thumbs_dir=temp_dir_name
-        )
-
-        for i in thumbs:
-            print(i)
+            for i in thumbs:
+                print(i)
         print()
 
-    current_time = time.process_time()
-
-    delta_time = current_time - last_time
+    delta_time = time.monotonic() - last_time
 
     print("\n\n\n")
-    print("TOTALLY (time): %d ms, %d webms" % (delta_time*1000, len(webms)))
-    print("<MILLISECONDS> (time) PER WEBM:", delta_time*1000/len(webms))
+    print('TOTALLY (time): {} ms, {} webms'.format(delta_time * 1000, len(webms)))
+    print("<MILLISECONDS> (time) PER WEBM:", delta_time * 1000 / len(webms))
